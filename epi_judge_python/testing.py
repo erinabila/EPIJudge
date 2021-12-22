4035cc98a4b418124a410495de1553bce5722a3c
erinabila <erin.abila99@gmail.com>
2021-12-22 14:58:45 -0500

    yay good job on variant testing

--- epi_judge_python/testing.py
+++ epi_judge_python/testing.py
@@ -1,8 +1,23 @@
 import copy
+from typing import List
 
-A = [1, 2, 3, 4, 5, 6, 7, 8]
+def longest_equal_subarray(A: List[int])-> int:
+    # TODO: Write a program that takes an array of integers and finds the length of a longest subarray all of whose entries are equal
+    window_start, window_end = 0, 0
+    length = 0
+    for i in range(len(A) - 1):
+        if A[i] == A[i + 1]:
+            window_end += 1
+        else:
+            length = max(length, window_end - window_start + 1)
+            window_start = i
+            window_end = i
+        print("Window start", window_start,  "   window end", window_end, " A[i]",   A[i])
+    return length
 
-for i in reversed(range(len(A))):
-    A[i] *= -1 
 
-print(A)
+
+
+
+A = [1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8]
+print(longest_equal_subarray(A))
