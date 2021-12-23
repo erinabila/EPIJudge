6cdb91665c6b390c484f365077f5722e1d8bd586
erinabila <erin.abila99@gmail.com>
2021-12-23 15:23:44 -0500

    testing string operations

--- epi_judge_python/testing.py
+++ epi_judge_python/testing.py
@@ -1,23 +1,27 @@
 import copy
 from typing import List
 
-def longest_equal_subarray(A: List[int])-> int:
-    # TODO: Write a program that takes an array of integers and finds the length of a longest subarray all of whose entries are equal
-    window_start, window_end = 0, 0
-    length = 0
-    for i in range(len(A) - 1):
-        if A[i] == A[i + 1]:
-            window_end += 1
-        else:
-            length = max(length, window_end - window_start + 1)
-            window_start = i
-            window_end = i
-        print("Window start", window_start,  "   window end", window_end, " A[i]",   A[i])
-    return length
+
+print('Euclid,Axiom 5, Parallel Lines'.split(','))
+
+print(3 * '01')
+
+print(','.join(('Gauss', 'Prince of Mathmeticians', '1777-1855')))
 
 
 
-
-
-A = [1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8]
-print(longest_equal_subarray(A))
+# def longest_equal_subarray(A: List[int])-> int:
+#     # TODO: Write a program that takes an array of integers and finds the length of a longest subarray all of whose entries are equal
+#     window_start, window_end = 0, 0
+#     length = 0
+#     for i in range(len(A) - 1):
+#         if A[i] == A[i + 1]:
+#             window_end += 1
+#         else:
+#             length = max(length, window_end - window_start + 1)
+#             window_start = i
+#             window_end = i
+#         print("Window start", window_start,  "   window end", window_end, " A[i]",   A[i])
+#     return length
+# A = [1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8]
+# print(longest_equal_subarray(A))
