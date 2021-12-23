0bbdc6f84753e37a78e676beda9336d6c66a79e9
erinabila <erin.abila99@gmail.com>
2021-12-23 15:05:45 -0500

    understands but looked at solution for O(1) space

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -1,13 +1,29 @@
 from test_framework import generic_test
 
-
+# TIME COMPLEXITY - O(N)
+# SPACE COMPLEXITY - O(1)
 def is_palindromic(s: str) -> bool:
-    # TODO - you fill in here.
+    # Rather than creating a new string for the reverse of the input string O(n),
+    # make it traverse the input string forwards and backwards to save space O(1)
+    
+    # Note that s[~i]for i in [0, len(s) - 1] is s[-(i + 1)]
+    # On a integer value is ~i = -(i + 1) = -i - 1
+
+    # EPI SOLUTION
+    # return all(s[i] == s[~i] for i in range(len(s) // 2))
+
+    # MY UNDERSTANDING FROM THE EPI SOLUTION
+    for i in range(len(s) // 2):
+        if s[i] != s[-i - 1]:
+            return False
+        print("s[", i, "]   ", s[i], "s[-", i, "- 1] ", s[~i])
     return True
 
-
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('is_string_palindromic.py',
-                                       'is_string_palindromic.tsv',
-                                       is_palindromic))
+    print(is_palindromic("abcba"))
+    print(is_palindromic("abcdfcba"))
+
+    # exit(
+    #     generic_test.generic_test_main('is_string_palindromic.py',
+    #                                    'is_string_palindromic.tsv',
+    #                                    is_palindromic))
