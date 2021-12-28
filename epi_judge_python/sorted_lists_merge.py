5a97b513d318a189d61e3a0e7c1233fa2d8eeeea
erinabila <erin.abila99@gmail.com>
2021-12-28 10:52:28 -0500

    bad! didn't consider creating a dummy node

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -3,14 +3,33 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
-
+# TIME COMPLEXITY: O(n + m)
+# SPACE COMPLEIXTY: O(1) since we resuse the existing nodes 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = tail = ListNode()
+
+    while L1 and L2:
+        if L1.data <= L2.data:
+            tail.next = L1
+            L1 = L1.next
+        else:
+            tail.next = L2
+            L2 = L2.next 
+
+        tail = tail.next 
+
+    # appends the remaining nodes of L1 or L2 
+    tail.next = L1 or L2 # cause EITHER OR list exists
+    return dummy_head.next # skips/removes dummy head
 
 
 if __name__ == '__main__':
+    # L1 = [2, 5, 7]
+    # L2 = [3, 11]
+
+    # print(merge_two_sorted_lists(L1, L2))
+
     exit(
         generic_test.generic_test_main('sorted_lists_merge.py',
                                        'sorted_lists_merge.tsv',
